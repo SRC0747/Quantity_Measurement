2e24aa28c563ec0e7acf43608c8d100d78a37a9f
SRC0747 <RjSrc@0747>
2021-09-21 14:07:36 +0530

    Added: Null-Reference-Type-Value Checking for Equality for FeetMeaurement.

--- src/test/java/com/bridgelabz_quantity_measurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz_quantity_measurement/QuantityMeasurementTest.java
@@ -1,7 +1,43 @@
 package com.bridgelabz_quantity_measurement;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityMeasurementTest {
 
+    @Test
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assertions.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1FeetNullValue_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1FeetReference_ShouldReturnEqual() {
+        Feet feet1 = new Feet(2.0);
+        Assertions.assertTrue(feet1.equals(feet1));
+    }
+
+    @Test
+    public void givenTypeCheckForFeet_ShouldReturnEquals() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assertions.assertTrue(feet1.getClass().equals(feet2.getClass()));
+    }
 }
